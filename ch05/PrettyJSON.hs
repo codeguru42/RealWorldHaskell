03d5868dd230bd4d50aada54c678a42a805b9a8b
codeguru <codeguru@users.sourceforge.net>
2013-08-26 19:03:08 -0600

    PrettyJSON: Partially implement 'renderJValue :: JValue -> Doc'
    Implement 'renderJValue :: JValue -> Doc' for JBool, JNull,
    JNumber, and JString. Stub out Doc data type and
    'string :: String -> Doc', 'text :: String -> Doc', and
    'double :: Double -> Doc' functions.

---
+++ ch05/PrettyJSON.hs
@@ -0,0 +1,24 @@
+module PrettyJSON where
+
+import SimpleJSON
+
+import Data.List (intercalate)
+
+renderJValue :: JValue -> Doc
+renderJValue (JBool True)  = text "true"
+renderJValue (JBool False) = text "false"
+renderJValue (JNull)       = text "null"
+renderJValue (JNumber num) = double num
+renderJValue (JString str) = string str
+
+data Doc = ToBeDefined
+    deriving (Show)
+
+string :: String -> Doc
+string str = undefined
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
