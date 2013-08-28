af3e2b0db955e81de2610a95314c3fb895d6a190
codeguru <codeguru@users.sourceforge.net>
2013-08-27 18:51:50 -0600

    PrettyJSON: Implement 'oneChar :: Char -> Doc'
    Implement 'oneChar :: Char -> Doc' and 'simpleEscapes :: [(Char, String)]'.
    Stub out 'hexEscape :: Char -> Doc'.

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -26,11 +26,22 @@ string = enclose '"' '"' . hcat . map oneChar
 enclose :: Char -> Char -> Doc -> Doc
 enclose left right x = char left <> x <> char right
 
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+    Just r -> text r
+    Nothing | mustEscape c -> hexEscape c
+            | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
 char :: Char -> Doc
 char c = undefined
 
-oneChar :: Char -> Doc
-oneChar c = undefined
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\', b])
+
+hexEscape :: Char -> Doc
+hexEscape = undefined
 
 text :: String -> Doc
 text str = undefined
