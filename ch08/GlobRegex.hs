beb72bff00b188ed722247a378de2728b8fb756b
codeguru <codeguru@users.sourceforge.net>
2013-11-12 20:43:11 -0700

    GlobRegex: Stubs for globToRegex and matchesGlob

---
+++ ch08/GlobRegex.hs
@@ -0,0 +1,11 @@
+module GlobRegex
+    ( globToRegex
+    , matchesGlob
+    ) where
+
+import Text.Regex.Posix ((=~))
+
+globToRegex :: String -> String
+globToRegex = undefined
+
+matchesGlob = undefined
