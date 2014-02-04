defe3003a85f09f01b9b4ef6478cdc7694e952c8
codeguru <codeguru@users.sourceforge.net>
2014-02-03 22:12:03 -0700

    First implementation of `parseP5`

--- ch10/PNM.hs
+++ ch10/PNM.hs
@@ -22,4 +22,24 @@ getBytes :: Int -> L.ByteString -> Maybe (L.ByteString, L.ByteString)
 getBytes = undefined
 
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
-parseP5 = undefined
+parseP5 s = case matchHeader (L8.pack "P5") s of
+    Nothing -> Nothing
+    Just s1 ->
+        case getNat s1 of
+            Nothing -> Nothing
+            Just (width, s2) ->
+                case getNat (L8.dropWhile isSpace s2) of
+                    Nothing -> Nothing
+                    Just (height, s3) ->
+                        case getNat (L8.dropWhile isSpace s3) of
+                            Nothing -> Nothing
+                            Just (maxGrey, s4)
+                                | maxGrey > 255 -> Nothing
+                                | otherwise ->
+                                    case getBytes 1 s4 of
+                                        Nothing -> Nothing
+                                        Just (_, s5) ->
+                                            case getBytes (width * height) s5 of
+                                                Nothing -> Nothing
+                                                Just (bitmap, s6) ->
+                                                    Just (Greymap width height maxGrey bitmap, s6)
