2694e62bc587c76ea29d1e895be1a3856664dacf
codeguru <codeguru@users.sourceforge.net>
2014-02-03 22:32:33 -0700

    Remove some boilerplate

--- ch10/PNM.hs
+++ ch10/PNM.hs
@@ -12,6 +12,10 @@ data Greymap = Greymap {
 instance Show Greymap where
     show (Greymap w h m _) = "Greymap " ++ show w ++ "x" ++ show h ++ " " ++ show m
 
+(>>?) :: Maybe a -> (a -> Maybe b) -> Maybe b
+Nothing >>? _ = Nothing
+Just v  >>? f = f v
+
 matchHeader :: L.ByteString -> L.ByteString -> Maybe L.ByteString
 matchHeader prefix str
     | prefix `L8.isPrefixOf` str
@@ -34,24 +38,17 @@ getBytes n str = let count            = fromIntegral n
                     else Just both
 
 parseP5 :: L.ByteString -> Maybe (Greymap, L.ByteString)
-parseP5 s = case matchHeader (L8.pack "P5") s of
-    Nothing -> Nothing
-    Just s1 ->
-        case getNat s1 of
-            Nothing -> Nothing
-            Just (width, s2) ->
-                case getNat (L8.dropWhile isSpace s2) of
-                    Nothing -> Nothing
-                    Just (height, s3) ->
-                        case getNat (L8.dropWhile isSpace s3) of
-                            Nothing -> Nothing
-                            Just (maxGrey, s4)
-                                | maxGrey > 255 -> Nothing
-                                | otherwise ->
-                                    case getBytes 1 s4 of
-                                        Nothing -> Nothing
-                                        Just (_, s5) ->
-                                            case getBytes (width * height) s5 of
-                                                Nothing -> Nothing
-                                                Just (bitmap, s6) ->
-                                                    Just (Greymap width height maxGrey bitmap, s6)
+parseP5 s =
+    matchHeader (L8.pack "P5") s      >>?
+    \s -> skipSpace ((), s)           >>?
+    (getNat . snd)                    >>?
+    skipSpace                         >>?
+    \(width, s) -> getNat s           >>?
+    skipSpace                         >>?
+    \(height, s) -> getNat s          >>?
+    \(maxGrey, s) -> getBytes 1 s     >>?
+    (getBytes (width * height) . snd) >>?
+    \(bitmap, s6) -> Just (Greymap width height maxGrey bitmap, s)
+
+skipSpace :: (a, L.ByteString) -> Maybe (a, L.ByteString)
+skipSpace (a, s) = Just (a, L8.dropWhile isSpace s)
